755214d2ec4d8d07d71ec7fc266718be67407edc
vuongvu <lw80945@linecorp.com>
2019-03-21 11:35:04 +0700

    prepare some facebook account

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const CRED = require('./creds');
+const fs = require('fs');
 
 const sleep = async (ms) => {
   return new Promise((res, rej) => {
@@ -25,21 +26,46 @@ const ID = {
     await page.goto('https://facebook.com', {
       waitUntil: 'networkidle2'
     });
-    await page.waitForSelector(ID.login);
-    console.log(CRED.user);
-    console.log(ID.login);
-    await page.type(ID.login, CRED.user);
+     await sleep(500);
+     await page.waitForSelector(ID.login);
+     console.log(CRED.user);
+     console.log(ID.login);
+     await page.type(ID.login, CRED.user);
+     await page.type(ID.pass, CRED.pass);
+     await sleep(500);
 
-    await page.type(ID.pass, CRED.pass);
-    await sleep(500);
+     await Promise.all([
+       page.waitForNavigation({ waitUntil: 'load' }),
+       page.click("#loginbutton")
+     ]);
 
-    await page.click("#loginbutton")
 
-    console.log("login done");
-    await page.waitForNavigation();
+     //console.log(">>>>>>>>>>>login done");
+     //await page.waitForNavigation();
   }
   await login();
+
+  console.log('>>>>>>>>>>>cookies : '+  JSON.stringify(await page.cookies()))
+  fs.writeFileSync('cookies_name.json', JSON.stringify(await page.cookies()));
+
+  /*  await page.goto('https://facebook.com', {
+        waitUntil: 'networkidle2'
+    });
+
+   let cookiesArr =  JSON.parse(fs.readFileSync('cookies_name.json').toString()) ;
+   if (cookiesArr.length !== 0) {
+      for (let cookie of cookiesArr) {
+        await page.setCookie(cookie)
+      }
+      console.log('Session has been loaded in the browser')
+   }
+
+   await page.goto('https://facebook.com', {
+           waitUntil: 'networkidle2'
+       });
+
   await page.screenshot({
     path: 'facebook.png'
-  });
+  });*/
+
 })();
